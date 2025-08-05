87ff11cc7b4b1d136bee6bbcad364e0db10bd647
Luqmanul Hakiem <luqmanul@ebconnect.com>
2025-08-05 10:20:30 +0700

    feat: comment unused assets

--- resources/views/src/auth/login.blade.php
+++ resources/views/src/auth/login.blade.php
@@ -27,10 +27,6 @@
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,800" rel="stylesheet" />
     <!-- Nucleo Icons -->
-    <link href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <!-- CSS Files -->
     <link id="pagestyle" href="{{ asset('assets/css/soft-ui-dashboard.css?v=1.1.0') }}" rel="stylesheet" />
     <!-- Nepcha Analytics (nepcha.com) -->
--- resources/views/src/layouts/footer.blade.php
+++ resources/views/src/layouts/footer.blade.php
@@ -1,15 +1,16 @@
 <footer class="footer pt-3  ">
-  <div class="container-fluid">
-    <div class="row align-items-center justify-content-lg-between">
-      <div class="col-lg-6 mb-lg-0 mb-4">
-        <div class="copyright text-center text-sm text-muted text-lg-start">
-          © <script>
-            document.write(new Date().getFullYear())
-          </script>,
-          Ayam Fin Dashboard.
-        </div>
-      </div>
-      {{-- <div class="col-lg-6">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+                <div class="copyright text-center text-sm text-muted text-lg-start">
+                    ©
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>,
+                    AyamFin Dashboard.
+                </div>
+            </div>
+            {{-- <div class="col-lg-6">
         <ul class="nav nav-footer justify-content-center justify-content-lg-end">
           <li class="nav-item">
             <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
@@ -25,6 +26,6 @@
           </li>
         </ul>
       </div> --}}
+        </div>
     </div>
-  </div>
 </footer>
